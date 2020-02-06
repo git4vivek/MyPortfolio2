76bbb9b054a777bbe068b985d949cf004b405386
git4vivek <agarwal.vivek101@gmail.com>
2020-02-06 13:55:39 -0700

    Updates

---
+++ precache-manifest.b290fc42a5c47b4f6cab7c00de034666.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc274a4b5bacf3eb14c6d11a725d1e6",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "62465a88e9f8e1358cec",
+    "url": "/MyPortfolio/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "c3b834707740b14e40d3",
+    "url": "/MyPortfolio/static/css/main.00559aaf.chunk.css"
+  },
+  {
+    "revision": "62465a88e9f8e1358cec",
+    "url": "/MyPortfolio/static/js/2.efed1f8a.chunk.js"
+  },
+  {
+    "revision": "8956eb797ec1ca27a54b67665871977c",
+    "url": "/MyPortfolio/static/js/2.efed1f8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3b834707740b14e40d3",
+    "url": "/MyPortfolio/static/js/main.dc4e1fb6.chunk.js"
+  },
+  {
+    "revision": "d00c7006c887eccf713c",
+    "url": "/MyPortfolio/static/js/runtime-main.7ac04cb1.js"
+  }
+]);
